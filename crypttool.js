700ad866bcce1d6e38312b4fd090ffea3b7cc76a
7184c6a22e3 <123277658+7184c6a22e3@users.noreply.github.com>
2023-01-23 21:48:21 +0100

    Update crypttool.js

--- crypttool.js
+++ crypttool.js
@@ -386,7 +386,7 @@ const CryptTool = (function () {
         try {
             plaintext = await window.crypto.subtle.decrypt(
                 cryptoSettings(adataString, spec),
-                await deriveKey(key, password, spec),
+                deriveKey(key, password, spec),
                 stringToArraybuffer(
                     atob(cipherMessage)
                 )
