f007929792b02901a1772c6b142073a52e1887ed
7184c6a22e3 <123277658+7184c6a22e3@users.noreply.github.com>
2023-01-31 16:12:36 +0100

    Update VaultManager.js

--- VaultManager.js
+++ VaultManager.js
@@ -1,4 +1,10 @@
 // Vault Importer
+function DecodeBase64(str) {
+    return decodeURIComponent(atob(str).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
 function FormatPasteData(data) {
     return [data.ct, [data.adata]]
 }
